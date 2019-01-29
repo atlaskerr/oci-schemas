85eecc4aba3230435c85657784e58d753e244cc1
Atlas Kerr <atlaskerr@gmail.com>
2019-01-29 10:06:56 -0600

    Rename go package name from `oci` to `schema`.
    Signed-off-by: Atlas Kerr <atlaskerr@gmail.com>

--- gen.go
+++ gen.go
@@ -1,3 +1,3 @@
-package oci
+package schema
 
-//go:generate esc -o=fs.go -modtime=1546544639 -pkg=oci -include=.*\.json$ -ignore=test-fixtures/* .
+//go:generate esc -o=fs.go -modtime=1546544639 -pkg=schema -include=.*\.json$ -ignore=test-fixtures/* .
